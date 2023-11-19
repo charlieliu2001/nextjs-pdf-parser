c0314b4fd8cf72710f87d0f9aaf7bc947a6e3031
Charlie Liu <charlieliu2001@yahoo.com>
2023-11-18 21:56:56 -0500

    fixed syntax

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,21 @@ import { FilePond } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 
 export default function Home() {
+  const [parsedPdfText, setParsedPdfText] = useState('');
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='w-96'>
         <FilePond
+          onprocessfiles={() => setParsedPdfText}
           server={{
             process: '/api/upload',
             fetch: null,
             revert: null,
           }}
         />
+      </div>
+      <div className='parsed-pdf-content'>
+        {parsedPdfText ? <div>{parsedPdfText}</div> : <p>No PDF data</p>}
       </div>
     </main>
   );
