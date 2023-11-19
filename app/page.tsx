024d9db70bee14557042554a68f88529ae037dcc
Charlie Liu <charlieliu2001@yahoo.com>
2023-11-18 20:41:18 -0500

    change to use FilePond directly

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+
+import React, { useState } from 'react';
 import FileUpload from '@/components/file-upload';
+import { FilePond } from 'react-filepond';
+import 'filepond/dist/filepond.min.css';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='w-96'>
-        <FileUpload />
+        <FilePond
+          server={{
+            process: '/api/upload',
+            fetch: null,
+            revert: null,
+          }}
+        />
       </div>
     </main>
   );
